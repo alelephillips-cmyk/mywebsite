f5c4de7763e7bec2ce721c97d6c27481b5f0f554
alelephillips-cmyk <alelephillips@gmail.com>
2026-09-08 01:24:41 +0300

    add password protection

--- server.js
+++ server.js
@@ -4,6 +4,16 @@ const { default: makeWASocket, useMultiFileAuthState, DisconnectReason } = requi
 const fs = require('fs');
 
 const app = express();
+app.use((req, res, next) => {
+  const auth = { login: 'kiuby', password: 'SILENT DEMONS' };
+  const b64auth = (req.headers.authorization || '').split(' ')[1] || '';
+  const [login, password] = Buffer.from(b64auth, 'base64').toString().split(':');
+  if (login === auth.login && password === auth.password) {
+    return next();
+  }
+  res.set('WWW-Authenticate', 'Basic realm="Restricted"');
+  res.status(401).send('Authentication required.');
+});
 app.use(express.json());
 app.use(express.static('public'));
 
